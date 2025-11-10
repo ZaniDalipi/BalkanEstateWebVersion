6e8787bf9130f11743e857be9eb6eb8bb1919e12
ZaniDalipi <zanoin@gmail.com>
2025-11-10 21:59:22 +0100

    somet

--- components/BuyerFlow/MapComponent.tsx
+++ components/BuyerFlow/MapComponent.tsx
@@ -11,8 +11,8 @@ import { BellIcon, PencilIcon, XCircleIcon } from '../../constants';
 let DefaultIcon = L.icon({
     iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
     shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
+    iconSize: [25, 25],
+    iconAnchor: [12, 25],
     popupAnchor: [1, -34],
     shadowSize: [41, 41]
 });
