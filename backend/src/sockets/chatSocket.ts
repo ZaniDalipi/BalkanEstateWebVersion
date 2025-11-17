731db3f70a3ec6f7b4f9dccaa63bd7cf46b36cc3
ZaniDalipi <zanoin@gmail.com>
2025-11-17 11:54:14 +0100

    "syntax error had to be fixed"

--- backend/src/sockets/chatSocket.ts
+++ backend/src/sockets/chatSocket.ts
@@ -1,6 +1,6 @@
 import { Server, Socket } from 'socket.io';
 import jwt from 'jsonwebtoken';
-import { IUser } from '../models/User';
+
 
 interface AuthenticatedSocket extends Socket {
   userId?: string;
